590e5e5d19db066df45d803fbabd109f40eda0c5
Rendall Schijven <rendall.schijven@hotmail.com>
2018-03-24 12:10:36 +0100

    latest

---
+++ cmake-build-debug/CMakeFiles/four-in-a-row.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/four-in-a-row.dir/main.cpp.o"
+  "CMakeFiles/four-in-a-row.dir/c4.cpp.o"
+  "CMakeFiles/four-in-a-row.dir/c4bot.cpp.o"
+  "four-in-a-row.pdb"
+  "four-in-a-row.exe"
+  "libfour-in-a-row.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/four-in-a-row.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
